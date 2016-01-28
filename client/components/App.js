c574683bae62962c689a1c88314d5a8f8b988ea8
iandeboisblanc <iandeboisblanc@gmail.com>
2016-01-27 16:49:56 -0800

    refactors App.js as class instantiation

--- client/components/App.js
+++ client/components/App.js
@@ -1,14 +1,24 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer videoObj={window.exampleVideoData[0]} />
-    </div>
-    <div className="col-md-5">
-      <VideoList />
-    </div>
-  </div>
-);
+class App extends React.Component {
+
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer videoObj={window.exampleVideoData[0]} />
+        </div>
+        <div className="col-md-5">
+          <VideoList />
+        </div>
+      </div>
+    )
+  }
+
+}
 
 
 window.App = App;
